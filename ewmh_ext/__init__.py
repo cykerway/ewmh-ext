dd613cbca634fd0c721221125d9ff24634e68957
Cyker Way <cykerway@gmail.com>
2018-12-03 02:36:50 -0500

    -

--- ewmh_ext/__init__.py
+++ ewmh_ext/__init__.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
 
+from ewmh import *
+
+def getWmName(self, win):
+    """
+    Get the property _NET_WM_NAME for the given window as a string.
+
+    :param win: the window object
+    :return: str
+    """
+    return self._getProperty('_NET_WM_NAME', win).decode()
+
+EWMH.getWmName = getWmName
 
